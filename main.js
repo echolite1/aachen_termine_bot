d8bb8c44c71815068e6000808de566cc32720d82
Ernest Hunko <echolite221@gmail.com>
2023-08-23 02:30:55 +0200

    major update
    it has 3 commands
    /fh
    /rwth
    /regular

--- main.js
+++ main.js
@@ -14,118 +14,132 @@ var time = dd + '-' + mm + '-' + yyyy + ' ' + today.getHours() + ':' + today.get
 const constantLink = 'https://termine.staedteregion-aachen.de/auslaenderamt/';
 const defaultTime = 1000;
 const multiplier = 4;
-// type variables
+const bot = new Telegraf(process.env.BOT_TOKEN);
+// citizen type variables
 const regular = {category: 115, buttons: [198, 201, 202], name: "Regular Person"};
 const studentFH = {category: 117, buttons: [193], name: "FH Student"};//https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/for...of
 const studentRWTH = {category: 114, buttons: [191], name: "RWTH Student"};
+var externalPersonType = "reg";
 // other functions
 function delay(time) {
     return new Promise(function(resolve) { setTimeout(resolve, time) });
 }
-
-const bot = new Telegraf(process.env.BOT_TOKEN);
-
-bot.start((ctx) => ctx.reply('Dear User, this bot can help you get the appointment. To start the bot type anything after this message.'));
-
-bot.on('message', async (ctx) => {
-    const response = 'Scan started. Available appointments:';
-    var currentUser = "" + ctx.chat.id;
-    findAppointment(bot, currentUser);
-    ctx.reply(`${response}`);
-});
-
+// bot commands
 bot.launch();
-
+bot.start((ctx) => ctx.reply('Dear User, this bot can help you get the appointment. To start bot type anything after this message.'));
+bot.command('fh', (ctx) => {
+    ctx.reply('Hello FH student!\nScan started. Available appointments:');
+    externalPersonType = "fh";
+    var currentUser = "" + ctx.chat.id;
+    findAppointment(bot, currentUser, externalPersonType);
+});
+bot.command('rwth', (ctx) => {
+    ctx.reply('Hello RWTH student!\nScan started. Available appointments:');
+    externalPersonType = "rwth";
+    var currentUser = "" + ctx.chat.id;
+    findAppointment(bot, currentUser, externalPersonType);
+});
+bot.command('regular', (ctx) => {
+    ctx.reply('Hello Citizen!\nScan started. Available appointments:');
+    externalPersonType = "reg";
+    var currentUser = "" + ctx.chat.id;
+    findAppointment(bot, currentUser, externalPersonType);
+});
 // ************************************** MAIN ******************************************************
-async function findAppointment(botEntity, currentUserID){
+async function findAppointment(botEntity, currentUserID, externalPersonType){
 
   const browser = await puppeteer.launch();//{headless: false, slowMo: 100}); // [_][_][_][_][_][_][_][_] );//
   const page = await browser.newPage();
   console.clear();
   console.log('- - - - - NEW SCAN ' + time + ' ID: ' + currentUserID + ' - - - - -');
 
-  await personType(regular.category, regular.buttons, regular.name);
-  // await personType(studentFH.category, studentFH.buttons, studentFH.name);
-  // await personType(studentRWTH.category, studentRWTH.buttons, studentRWTH.name);
+  switch (externalPersonType){
+    case "reg": await personType(regular.category, regular.buttons, regular.name);
+        break;
+    case "fh": await personType(studentFH.category, studentFH.buttons, studentFH.name);
+        break;
+    case "rwth": await personType(studentRWTH.category, studentRWTH.buttons, studentRWTH.name);
+        break;
+  }
 
   async function personType(categoryID, buttonType, personName) {
-          var link = constantLink;
-          await page.goto(link);
-          await delay(defaultTime);
-          
-          // pressing buttons section
-          [mainPage_FirstOption] = await page.$x('//*[@id="buttonfunktionseinheit-1"]');
-          await mainPage_FirstOption.evaluate(mainPage_FirstOption => mainPage_FirstOption.click());
-          console.log('p1 done');
-          await delay(defaultTime);
+        var link = constantLink;
+        await page.goto(link);
+        await delay(defaultTime);
+        
+        // pressing buttons section
+        [mainPage_FirstOption] = await page.$x('//*[@id="buttonfunktionseinheit-1"]');
+        await mainPage_FirstOption.evaluate(mainPage_FirstOption => mainPage_FirstOption.click());
+        console.log('p1 done');
+        await delay(defaultTime);
 
-          [secondPage_Aufenthalt] = await page.$x('//*[@id="header_concerns_accordion-' + categoryID + '\"]');
-          await secondPage_Aufenthalt.evaluate(secondPage_Aufenthalt => secondPage_Aufenthalt.click());
-          console.log('p2 dropdown');
-          await delay(defaultTime);
-          //new team
-          for(const element of buttonType){
-              [secondPage_T1Plus] = await page.$x('//*[@id="button-plus-' + element + '\"]');
-              await secondPage_T1Plus.evaluate(secondPage_T1Plus => secondPage_T1Plus.click());
-              console.log('p2 team selected');
-              await delay(defaultTime);
-  
-              [secondPage_Proceed] = await page.$x('//*[@id="WeiterButton"]');
-              await secondPage_Proceed.evaluate(secondPage_Proceed => secondPage_Proceed.click());
-              console.log('p2 proceed');
-              await delay(defaultTime);
-  
-              [secondPage_Popup] = await page.$x('//*[@id="OKButton"]');
-              await secondPage_Popup.evaluate(secondPage_Popup => secondPage_Popup.click());
-              console.log('p2 popup');
-              await delay(defaultTime * multiplier);
-  
-              [thirdPage_Location] = await page.$x('//*[@id="ui-id-2"]/form/table/tbody/tr[5]/td/input');
-              await thirdPage_Location.evaluate(thirdPage_Location => thirdPage_Location.click());
-              console.log('p3 location');
-              await delay(defaultTime * multiplier);
-              
-              console.log("\n" + personName);
-              try {
-                  await page.waitForSelector("#inhalt > div.info > p > strong > span", { timeout: 500 });
-  
-                  for(let i = 1; i < 15; i++){
-                      try{
-                          [thirdPage_Result] = await page.$x('//*[@id="ui-id-'+i+'\"]');
-                          result = await thirdPage_Result.getProperty('textContent');
-                          resultText = await result.jsonValue();
-                          if(resultText == "Vorschläge filtern"){
-                              i = 15;
-                          }
-                          else{
-                              console.log(resultText);
-                              botEntity.telegram.sendMessage(currentUserID, resultText);
-                          }
-                      }
-                      catch(error){
-                          console.error('Error: ' + error);
-                      }
-                  }
-  
-              } catch (error) {
-                  console.log("No appointments available");
-              }
-  
-              [thirdPage_Back] = await page.$x('//*[@id="zurueck"]');
-              await thirdPage_Back.evaluate(thirdPage_Back => thirdPage_Back.click());
-              console.log('\np3 Zurück');
-              await delay(defaultTime);
-  
-              [secondPage_Back] = await page.$x('//*[@id="zurueck"]');
-              await secondPage_Back.evaluate(secondPage_Back => secondPage_Back.click());
-              console.log('p2 Zurück');
-              await delay(defaultTime);
-  
-              [secondPage_T1Minus] = await page.$x('//*[@id="button-minus-' + element + '\"]');
-              await secondPage_T1Minus.evaluate(secondPage_T1Minus => secondPage_T1Minus.click());
-              console.log('p2 no team');
-              await delay(defaultTime);
-          }
+        [secondPage_Aufenthalt] = await page.$x('//*[@id="header_concerns_accordion-' + categoryID + '\"]');
+        await secondPage_Aufenthalt.evaluate(secondPage_Aufenthalt => secondPage_Aufenthalt.click());
+        console.log('p2 dropdown');
+        await delay(defaultTime);
+        //new team
+        for(const element of buttonType){
+            [secondPage_T1Plus] = await page.$x('//*[@id="button-plus-' + element + '\"]');
+            await secondPage_T1Plus.evaluate(secondPage_T1Plus => secondPage_T1Plus.click());
+            console.log('p2 team selected');
+            await delay(defaultTime);
+
+            [secondPage_Proceed] = await page.$x('//*[@id="WeiterButton"]');
+            await secondPage_Proceed.evaluate(secondPage_Proceed => secondPage_Proceed.click());
+            console.log('p2 proceed');
+            await delay(defaultTime);
+
+            [secondPage_Popup] = await page.$x('//*[@id="OKButton"]');
+            await secondPage_Popup.evaluate(secondPage_Popup => secondPage_Popup.click());
+            console.log('p2 popup');
+            await delay(defaultTime * multiplier);
+
+            [thirdPage_Location] = await page.$x('//*[@id="ui-id-2"]/form/table/tbody/tr[5]/td/input');
+            await thirdPage_Location.evaluate(thirdPage_Location => thirdPage_Location.click());
+            console.log('p3 location');
+            await delay(defaultTime * multiplier);
+            
+            console.log("\n" + personName);
+            try {
+                await page.waitForSelector("#inhalt > div.info > p > strong > span", { timeout: 500 });
+
+                for(let i = 1; i < 15; i++){
+                    try{
+                        [thirdPage_Result] = await page.$x('//*[@id="ui-id-'+i+'\"]');
+                        result = await thirdPage_Result.getProperty('textContent');
+                        resultText = await result.jsonValue();
+                        if(resultText == "Vorschläge filtern"){
+                            i = 15;
+                        }
+                        else{
+                            console.log(resultText);
+                            botEntity.telegram.sendMessage(currentUserID, resultText);
+                        }
+                    }
+                    catch(error){
+                        console.error('Error: ' + error);
+                    }
+                }
+
+            } catch (error) {
+                console.log("No appointments available");
+            }
+
+            [thirdPage_Back] = await page.$x('//*[@id="zurueck"]');
+            await thirdPage_Back.evaluate(thirdPage_Back => thirdPage_Back.click());
+            console.log('\np3 Zurück');
+            await delay(defaultTime);
+
+            [secondPage_Back] = await page.$x('//*[@id="zurueck"]');
+            await secondPage_Back.evaluate(secondPage_Back => secondPage_Back.click());
+            console.log('p2 Zurück');
+            await delay(defaultTime);
+
+            [secondPage_T1Minus] = await page.$x('//*[@id="button-minus-' + element + '\"]');
+            await secondPage_T1Minus.evaluate(secondPage_T1Minus => secondPage_T1Minus.click());
+            console.log('p2 no team');
+            await delay(defaultTime);
+        }
       
       return;
   }
